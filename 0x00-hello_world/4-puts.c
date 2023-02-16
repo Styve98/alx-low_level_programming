47b90af1915df950d9eccd29eaf5af4a2e6dd0a0
styve98 <stevekihiu5195@gmail.com>
2023-02-16 13:01:44 -0800

    using the puts.c function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program printing using the puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
