21f14175b01b09994fb331ce86d4f5bd8e9dd53f
styve98 <stevekihiu5195@gmail.com>
2023-02-23 03:19:35 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <ctype.h>
+
 /**
  * main - check the code.
  *
@@ -11,8 +11,8 @@ int main(void)
 char c;
 
 c = 'A';
-printf("%c: %d\n", c, isupper(c));
+printf("%c: %d\n", c, _isupper(c));
 c = 'a';
-printf("%c: %d\n", c, isupper(c));
+printf("%c: %d\n", c, _isupper(c));
 return (0);
 }
