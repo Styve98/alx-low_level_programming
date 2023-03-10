dc7c4be0f9b9d6fdf33bce9556d4f2fe37cc08e1
styve98 <stevekihiu5195@gmail.com>
2023-03-10 06:32:57 -0800

    create_static_lib.sh

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -11,7 +11,6 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-
 	i = 0;
 	while (dest[i] != '\0')
 	{
@@ -24,7 +23,6 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
-
 
 	dest[i] = '\0';
 	return (dest);
